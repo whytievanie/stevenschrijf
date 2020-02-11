003c45ac673a6845c56a0041b5b291a9a88aaf23
Esma Yilmaz <Esma_yilmaz7@hotmail.com>
2020-02-11 15:24:47 +0100

    De seeder goed gemaakt

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -11,25 +11,25 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        \App\product::insert([
+        \App\Model\Product::insert([
             'image' => '/storage/img/Allesmag.png',
             'name' => 'Alles mag',
             'price' => '22.50'
          ]);
 
-         \App\product::insert([
+         \App\Model\Product::insert([
             'image' => '/storage/img/dewereld.png',
             'name' => 'De wereld',
             'price' => '24.00'
          ]);
 
-         \App\product::insert([
+         \App\Model\Product::insert([
             'image' => '/storage/img/dewaarheid.png',
             'name' => 'De waarheid',
             'price' => '22.50'
          ]);
 
-         \App\product::insert([
+         \App\Model\product::insert([
             'image' => '/storage/img/magdit.png',
             'name' => 'Mag dit?',
             'price' => '24.00'
